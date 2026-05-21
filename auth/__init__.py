1b69d9e542942b33b3932ebc4df7950f5823c707
batirniyaz <muratbaevbatirniyaz@gmail.com>
2026-05-21 13:21:48 +0500

    include mentor router in main API

--- auth/__init__.py
+++ auth/__init__.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter
 
 from auth.api import router as auth_router
 from auth.endpoint import router as user_router
+from auth.mentor_endpoint import router as mentor_router
 
 
 router = APIRouter()
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(user_router, prefix="/user", tags=["user"])
+router.include_router(mentor_router, prefix="/mentor", tags=["mentor"])
+
